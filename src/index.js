5d63ea4d737206b943bb6b672c5377aa2b682ac0
ishaban <ishaban.flash@gmail.com>
2021-03-13 21:07:13 +0300

    implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const activeConfig = [];
+    let hasError = false;
+    
+    str.split('').forEach((symbol) => {
+        if (hasError) {
+            return;
+        }
+        
+        if (activeConfig.length === 0 || symbol !== activeConfig[activeConfig.length - 1][1]) {
+            const config = bracketsConfig.find(([openBracket]) => {
+                return symbol === openBracket;
+            });
+            if (config) {
+                activeConfig.push(config);
+            } else {
+                hasError = true;
+            }
+        } else if (symbol === activeConfig[activeConfig.length - 1][1]) {
+            activeConfig.pop();
+        } else {
+            hasError = true;
+        }
+    });
+    return !hasError && activeConfig.length === 0;
+};
